6f3c2d3f32f18a254629a999dee2ebae7ee7aca7
An123Nav <naveen.an0523@gmail.com>
2026-07-18 19:01:15 +0530

    Port Changes

--- app.py
+++ app.py
@@ -174,6 +174,8 @@ if __name__ == "__main__":
         server_name="0.0.0.0",
         server_port=port,
         share=False,
+        show_error=True,
+        quiet=False,
     )
 
     print("14. Gradio launched", flush=True)
